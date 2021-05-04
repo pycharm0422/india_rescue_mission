9e6ca7bece74210a6c271c15ded3a04193a4fc50
Faraz <pycharm0422@gmail.com>
2021-05-04 23:56:17 +0530

    updateds

--- india_rescue_mission/settings.py
+++ india_rescue_mission/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 
